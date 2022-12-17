00b17fad3405e6ab4e2dbc2b582941801afe2d73
Burak-Pehlivan <burak.pehlivan@windowslive.com>
2022-12-18 01:16:19 +0300

    java_constructors2

---
+++ src/Java_constructors1/c6_thisKullanimi.java
@@ -0,0 +1,17 @@
+package Java_constructors1;
+
+public class c6_thisKullanimi {
+
+    // basina konuldugu variable'in instance variable oldugunu belirtmek icin kullanilir.
+    String isim;
+    String soyisim;
+    String brans;
+    public c6_thisKullanimi(String ism, String soyism, String brns){
+
+        isim = ism;
+        soyisim = soyism;
+        brans = brns;
+
+    }
+
+}
