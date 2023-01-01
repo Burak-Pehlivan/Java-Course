b955f8737b45596203eada1893fcf6fd2257c6fc
Burak-Pehlivan <burak.pehlivan@windowslive.com>
2023-01-01 22:07:08 +0300

    Instance ve Static Variables

---
+++ src/Java_Variables/c1_variables.java
@@ -0,0 +1,16 @@
+package Java_Variables;
+
+public class c1_variables {
+
+    private static int iAmClassVariable;
+    private int iAmInstanceVariable;
+
+
+    public static void main(String args[]) {
+        int iAmLocalVariable = 3;
+        System.out.println("class variable: " + iAmClassVariable);
+        System.out.println("instance variable: " + iAmInstanceVariable);   // static olmadigi icin kirmizi cizdi.
+        System.out.println("local variable: " + iAmLocalVariable);
+
+    }
+}
